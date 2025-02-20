c638a4418a6570e755df2b6b2362b6674304689e
Virginiiamz <105070914+Virginiiamz@users.noreply.github.com>
2025-02-20 14:28:42 +0100

    Actualizado

--- config/config.js
+++ config/config.js
@@ -3,13 +3,13 @@ require("dotenv").config({
 })
 
 module.exports = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 80,
   db: {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "yamabiko.proxy.rlwy.net",
     user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "test",
+    password: process.env.DB_PASSWORD || "dKjxnYeLeANjNrgvakSXBiKLJyHHTfUY",
     name: process.env.DB_NAME || "ligafemenina",
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 39681,
   },
   secretKey: process.env.SECRET_KEY || "default_secret",
 };
